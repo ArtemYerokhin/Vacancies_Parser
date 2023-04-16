637403fbd9a13d309c1307a6f412259eb1545d9a
ArtemYerokhin <110336811+ArtemYerokhin@users.noreply.github.com>
2023-04-16 16:23:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from headhunter import get_jobs as get_hh_jobs
 from habr import get_jobs as get_habr_jobs
-from save import save_to_csv
+import csv
 
 
 if __name__ == '__main__':
-  hh_jobs = get_hh_jobs()
-  habr_jobs = get_habr_jobs()
-  jobs = hh_jobs + habr_jobs
-  save_to_csv(jobs)
+    hh_jobs = get_hh_jobs()
+    habr_jobs = get_habr_jobs()
+    jobs = hh_jobs + habr_jobs
+
+    file = open('vacancies.csv', mode='w', encoding='utf-8-sig', newline='')
+    writer = csv.writer(file)
+    writer.writerow(['Заголовок', 'Компания', 'Информация', 'Ссылка'])
+    for job in jobs:
+        writer.writerow(list(job.values()))
+    file.close()
+    print('Файл vacancies.csv успешно записан')
