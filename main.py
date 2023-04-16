251d9f329ed64d818b68bde1c520cfaffd4c9c50
ArtemYerokhin <110336811+ArtemYerokhin@users.noreply.github.com>
2023-04-16 14:50:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from habr import get_jobs as get_habr_jobs
 from save import save_to_csv
 
 
-hh_jobs = get_hh_jobs()
-habr_jobs = get_habr_jobs()
-
-jobs = hh_jobs + habr_jobs
-save_to_csv(jobs)
+if __name__ == '__main__':
+  hh_jobs = get_hh_jobs()
+  habr_jobs = get_habr_jobs()
+  jobs = hh_jobs + habr_jobs
+  save_to_csv(jobs)
