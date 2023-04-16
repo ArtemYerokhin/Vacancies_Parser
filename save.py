61076870ccf4f3cca79838812452f8df7aeb7330
ArtemYerokhin <110336811+ArtemYerokhin@users.noreply.github.com>
2023-04-16 14:42:36 +0300

    Update save.py

--- save.py
+++ save.py
@@ -1,9 +1,9 @@
 import csv
 
 def save_to_csv(jobs):
-  file = open('test.csv', mode='w')
+  file = open('vacancies.csv', mode='w')
   writer = csv.writer(file)
-  writer.writerow(['title', 'company', 'info', 'link'])
+  writer.writerow(['Заголовок', 'Компания', 'Информация', 'Ссылка'])
   for job in jobs:
     writer.writerow(list(job.values()))
   return
